b681298af0c6266347ba4bbfdf41118fffc882d0
Jamie Xu <chengx2@illinois.edu>
2018-12-09 22:50:25 -0600

    yay

--- reverse_kin/reverse_kin.ino
+++ reverse_kin/reverse_kin.ino
@@ -36,56 +36,59 @@ Servos 1, 3, 5: normal (+ = up, - = down)
 */
 void setup()
 {
-
-//Serial.begin(9600);
-for(int i = 0; i < 6; i++)
-{
-servo[i].attach(servo_pin[i]);
-servo[i].writeMicroseconds(servo_zero[i]);
-}
-delay(1000);
+  Serial.begin(9600);
+  for(int i = 0; i < 6; i++)
+    {
+    servo[i].attach(servo_pin[i]);
+    servo[i].writeMicroseconds(servo_zero[i]);
+    }
+  delay(1000);
 }
 void loop()
-{
-/*  float z = 0;
+  {
+  /*  float z = 0;
   while(!Serial.available()){
-}
- z = Serial.read();
- Serial.print(z);
- */
-static float pe[6] = {0,10,0,radians(0),radians(0),radians(0)}, theta_a[6], servo_pos[6],q[3][6], r[3][6], dl[3][6], dl2[6];
-/*
-pe = location and orientation of end effector frame relative to the base frame [sway, surge,heave, pitch, roll, yaw)
-theta_a = angle of the servo arm
-servo_pos = value written to each servo
-q = position of lower mounting point of connecting link [x,y,x][1-6]
-r = position of upper mounting point of connecting link
-dl = dierence between x,y,z coordinates of q and r
-dl2 = distance between q and r
-*/
-for(int i = 0; i < 6; i++)
-{
-q[0][i] = L1*cos(-theta_a[i])*cos(theta_s[i]) + p[0][i];
-q[1][i] = L1*cos(-theta_a[i])*sin(theta_s[i]) + p[1][i];
-q[2][i] = -L1*sin(-theta_a[i]);
-r[0][i] = re[0][i]*cos(pe[4])*cos(pe[5]) + re[1][i]*(sin(pe[3])*sin(pe[4])*cos(pe[5]) -
-cos(pe[3])*sin(pe[5])) + pe[0];
-r[1][i] = re[0][i]*cos(pe[4])*sin(pe[5]) + re[1][i]*(cos(pe[3])*cos(pe[5]) +
-sin(pe[3])*sin(pe[4])*sin(pe[5])) + pe[1];
-r[2][i] = -re[0][i]*sin(pe[4]) + re[1][i]*sin(pe[3])*cos(pe[4]) + z_home + pe[2];
-dl[0][i] = q[0][i] - r[0][i];
-dl[1][i] = q[1][i] - r[1][i];
-dl[2][i] = q[2][i] - r[2][i];
-dl2[i] = sqrt(dl[0][i]*dl[0][i] + dl[1][i]*dl[1][i] + dl[2][i]*dl[2][i]) - L2;
-theta_a[i] += dl2[i];
-//define direction of rotation
-theta_a[i] = constrain(theta_a[i], servo_min, servo_max);
-if(i%2 == 1) servo_pos[i] = servo_zero[i] + theta_a[i]*servo_mult;
-else servo_pos[i] = servo_zero[i] - theta_a[i]*servo_mult;
-}
-//write the angels to the servo
-for(int i = 0; i < 6; i++)
-{
-servo[i].writeMicroseconds(servo_pos[i]);
-}
-}
+  }
+  z = Serial.read();
+  Serial.print(z);
+  */
+  static float pe[6] = {0,0,10,radians(0),radians(0),radians(0)}, theta_a[6], servo_pos[6],q[3][6], r[3][6], dl[3][6], dl2[6];
+  /*
+  pe = location and orientation of end effector frame relative to the base frame [sway, surge,heave, pitch, roll, yaw)
+  theta_a = angle of the servo arm
+  servo_pos = value written to each servo
+  q = position of lower mounting point of connecting link [x,y,x][1-6]
+  r = position of upper mounting point of connecting link
+  dl = dierence between x,y,z coordinates of q and r
+  dl2 = distance between q and r
+  */
+  for(int i = 0; i < 6; i++)
+    {
+    q[0][i] = L1*cos(-theta_a[i])*cos(theta_s[i]) + p[0][i];
+    q[1][i] = L1*cos(-theta_a[i])*sin(theta_s[i]) + p[1][i];
+    q[2][i] = -L1*sin(-theta_a[i]);
+    r[0][i] = re[0][i]*cos(pe[4])*cos(pe[5]) + re[1][i]*(sin(pe[3])*sin(pe[4])*cos(pe[5]) -
+    cos(pe[3])*sin(pe[5])) + pe[0];
+    r[1][i] = re[0][i]*cos(pe[4])*sin(pe[5]) + re[1][i]*(cos(pe[3])*cos(pe[5]) +
+    sin(pe[3])*sin(pe[4])*sin(pe[5])) + pe[1];
+    r[2][i] = -re[0][i]*sin(pe[4]) + re[1][i]*sin(pe[3])*cos(pe[4]) + z_home + pe[2];
+    dl[0][i] = q[0][i] - r[0][i];
+    dl[1][i] = q[1][i] - r[1][i];
+    dl[2][i] = q[2][i] - r[2][i];
+    dl2[i] = sqrt(dl[0][i]*dl[0][i] + dl[1][i]*dl[1][i] + dl[2][i]*dl[2][i]) - L2;
+    theta_a[i] += dl2[i];
+    //define direction of rotation
+    theta_a[i] = constrain(theta_a[i], servo_min, servo_max);
+    if(i%2 == 1) servo_pos[i] = servo_zero[i] + theta_a[i]*servo_mult;
+      else servo_pos[i] = servo_zero[i] - theta_a[i]*servo_mult;
+/*    }
+    //write the angels to the servo
+  for(int i = 0; i < 6; i++)
+    {
+    */
+    Serial.print(servo_pos[i]);
+    Serial.print("    ");
+    if(i==5) Serial.println();
+    servo[i].writeMicroseconds(servo_pos[i]);
+    }
+  }
